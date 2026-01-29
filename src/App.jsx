7f2f04ecc76a1672ce4c50aa8d285c46fb5a17d7
IorYohanna <yorakotosandratana@gmail.com>
2026-01-29 13:08:08 +0300

    Initialize project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue text-white text-3xl">
+      Tailwind v4 is working 🔥
+    </div>
+  )
+}
+
